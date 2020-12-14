5ac591c3243842c9381e02c89ae4fab970fc0cd0
Nate Birth <nate.birth@gmail.com>
2020-12-14 16:26:26 -0700

    Added payment processing Code to model

--- portfolio_proj/apps/store/models.py
+++ portfolio_proj/apps/store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+
+import stripe
 
 class Product(models.Model):
     name = models.CharField(max_length=45)
@@ -42,3 +45,30 @@ class Address(models.Model):
     order = models.ForeignKey(Order, related_name="address", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def charge(self, request, email, fee):
+        # Set your secret key: remember to change this to your live secret key
+        # in production. See your keys here https://manage.stripe.com/account
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+        # Get the credit card details submitted by the form
+        token = request.POST['stripeToken']
+
+        # Create a Customer
+        stripe_customer = stripe.Customer.create(
+            card=token,
+            description=email
+        )
+
+        # Save the Stripe ID to the customer's profile
+        self.stripe_id = stripe_customer.id
+        self.save()
+
+        # Charge the Customer instead of the card
+        stripe.Charge.create(
+            amount=fee, # in cents
+            currency="usd",
+            customer=stripe_customer.id
+        )
+
+        return stripe_customer
